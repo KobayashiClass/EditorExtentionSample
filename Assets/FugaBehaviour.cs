1a6dd0680404540a995fece5d7477fdc745b6e9e
kobayashi <traitblow@gmail.com>
2021-11-06 15:46:21 +0900

    テキスト系サンプル追加

--- Assets/FugaBehaviour.cs
+++ Assets/FugaBehaviour.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FugaBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Text m_text;
+
+    private void Start()
     {
-        
+        m_text = GetComponent<Text>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetText(string str)
     {
-        
+        m_text.text = str;
+    }
+
+    public void SetTextAnimation(string str)
+    {
+        StartCoroutine(StepAnimation(str));
+    }
+
+    private IEnumerator StepAnimation(string str)
+    {
+        var current = "";
+        m_text.text = current;
+        foreach(var c in str)
+        {
+            current = current + c;
+            m_text.text = current;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        m_text.text = str;
     }
 }
